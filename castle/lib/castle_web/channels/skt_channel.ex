3713c8fb11b53223b96f278c2c6cd245c9a3ed8c
James <thejamesash1@gmail.com>
2021-03-09 18:28:25 -0500

    remove stuff

--- castle/lib/castle_web/channels/skt_channel.ex
+++ castle/lib/castle_web/channels/skt_channel.ex
@@ -11,13 +11,6 @@ defmodule CastleWeb.SktChannel do
     end
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  @impl true
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
-
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (skt:lobby).
   @impl true
