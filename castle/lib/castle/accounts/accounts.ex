8d8612c7cc85d794a910457811f2120ebaa57324
James <thejamesash1@gmail.com>
2021-03-10 09:23:32 -0500

    remove stuff

--- castle/lib/castle/accounts/accounts.ex
+++ castle/lib/castle/accounts/accounts.ex
@@ -2,7 +2,6 @@ defmodule Castle.Accounts do
     import Ecto.Query
 
     def auth_with_email_and_password(%{"email" => email, "pass" => pass}) do
-      IO.puts(Application.get_env(:castle, CastleWeb.Bcrypt)[:salt])
     end
 
     def register_email_and_password(%{"email" => email, "pass" => pass}) do
