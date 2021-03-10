26e2e94015aee6d23d5af5e4ba8eecc66f4d8556
James <thejamesash1@gmail.com>
2021-03-09 20:35:29 -0500

    remove stuff

--- castle/lib/castle.ex
+++ castle/lib/castle.ex
@@ -1,9 +1,3 @@
 defmodule Castle do
-  @moduledoc """
-  Castle keeps the contexts that define your domain
-  and business logic.
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
 end
