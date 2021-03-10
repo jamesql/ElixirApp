09736750a8c2d8977af09242ff2cffd2189cc56f
James <thejamesash1@gmail.com>
2021-03-09 19:41:48 -0500

    add heartbeat and heartbeat_ack

--- castle/assets/js/socket.js
+++ castle/assets/js/socket.js
@@ -2,16 +2,32 @@ import {Socket} from "phoenix"
 
 let socket = new Socket("/socket", {params: {token: window.userToken}})
 
+let hb;
+let last_hb;
+let hb_int;
+let sq = 0;
+
 socket.connect()
 
 let channel = socket.channel("skt:lobby", {})
 channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+  .receive("ok", resp => { console.log("[WS] Connected to WebSocket.", resp) })
+  .receive("error", resp => { console.log("[WS] Error connecting to WebSocket.", resp) })
 
-// Socket Message Handler
+// Socket Message Handlers
 channel.on("HELLO", payload => {
-  console.log(payload);
+  hb_int = Number(payload["heartbeatInterval"])
+  channel.push("HELLO_ACK")
+})
+
+channel.on("READY", payload => {
+  setInterval(()=> {
+    channel.push("HEARTBEAT", {s: ++sq})
+  }, hb_int)
+})
+
+channel.on("HEARTBEAT_ACK", payload => {
+  console.log("[WS] Heartbeat Acknowledged.")
 })
 
 
--- castle/lib/castle_web/channels/skt_channel.ex
+++ castle/lib/castle_web/channels/skt_channel.ex
@@ -4,25 +4,32 @@ defmodule CastleWeb.SktChannel do
   @impl true
   def join("skt:lobby", payload, socket) do
     if authorized?(payload) do
-      send(self, :after_join)
+      send(self(), :after_join)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
     end
   end
 
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (skt:lobby).
-  @impl true
-  def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
+  # HELLO_ACK
+  # Hello Acknowledgement, aka first heartbeat
+  # since auth is handled in join we no longer need a AUTH code
+  def handle_in("HELLO_ACK", payload, socket) do
+    push(socket, "READY", %{})
+    {:noreply, socket}
+  end
+
+  # HEARTBEAT
+  # Checks on the status of the client
+  def handle_in("HEARTBEAT", payload, socket) do
+    push(socket, "HEARTBEAT_ACK", %{})
     {:noreply, socket}
   end
 
   # Handle Newly Connected Sockets
   # Send a Hello Packet with HeartBeat Interval
   def handle_info(:after_join, socket) do
-    push(socket, "HELLO", %{heartbeatInterval: "3e5"})
+    push(socket, "HELLO", %{heartbeatInterval: "3e4"})
     {:noreply, socket} 
   end
 
