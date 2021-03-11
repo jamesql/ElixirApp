814b1a44416926e1cce117ce4ed0957f339fdd23
James <thejamesash1@gmail.com>
2021-03-11 16:49:52 -0500

    start working on database

--- castle/lib/castle/repo.ex
+++ castle/lib/castle/repo.ex
@@ -2,4 +2,10 @@ defmodule Castle.Repo do
   use Ecto.Repo,
     otp_app: :castle,
     adapter: Ecto.Adapters.Postgres
+
+  def start(_,_) do
+    # Start supervisor
+    children = [
+      Friends.Repo
+    ]
 end
