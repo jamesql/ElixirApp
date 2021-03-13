813ea3ce2a472098a25e77d3fb53458498ea5606
James <thejamesash1@gmail.com>
2021-03-13 13:44:38 -0500

    add register function

--- castle/lib/castle/accounts/accounts.ex
+++ castle/lib/castle/accounts/accounts.ex
@@ -1,10 +1,27 @@
 defmodule Castle.Accounts do
     import Ecto.Query
+    alias Castle.{Repo, Structures.User}
 
-    def auth_with_email_and_password(%{"email" => email, "pass" => pass}) do
+    def auth_with_email_and_password(%{"email" => e, "pass" => p}) do
+        d = User |> Repo.get_by(email: e)
+        cond do
+            d not nil -> 
+                # Auth User
+            d nil ->
+                # Reject User 
+                {:error, "Account not found with that email address."}
+        end
     end
 
-    def register_email_and_password(%{"email" => email, "pass" => pass}) do
+    def register_email_and_password(%{"email" => e, "pass" => p}) do
+        c = User |> Repo.get_by(email: e)
+        if c == nil do # Register User
+            s = Snowflake.next_id()
+            ep = Bcrypt.hash_pwd_salt(p)
+            {:ok, u} = Repo.insert(%User{"uid" => s, "email" => e, "password" => ep}) 
+        else # Respond With Error
+            {:error, "Account already registered with that email."}
+        end
     end
 
 end
