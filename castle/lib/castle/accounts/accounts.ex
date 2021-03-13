b8cd347435dc908a973332a5510c63400fff2be5
James <thejamesash1@gmail.com>
2021-03-13 14:02:47 -0500

    finish auth function

--- castle/lib/castle/accounts/accounts.ex
+++ castle/lib/castle/accounts/accounts.ex
@@ -5,9 +5,10 @@ defmodule Castle.Accounts do
     def auth_with_email_and_password(%{"email" => e, "pass" => p}) do
         d = User |> Repo.get_by(email: e)
         cond do
-            d not nil -> 
+            d !== nil -> 
                 # Auth User
-            d nil ->
+                if(p |> Bcrypt.verify_pass(d.password), do: {:ok, "Logged in!"}, else: {:error, "Incorrect Password."})
+            d == nil ->
                 # Reject User 
                 {:error, "Account not found with that email address."}
         end
@@ -18,7 +19,7 @@ defmodule Castle.Accounts do
         if c == nil do # Register User
             s = Snowflake.next_id()
             ep = Bcrypt.hash_pwd_salt(p)
-            {:ok, u} = Repo.insert(%User{"uid" => s, "email" => e, "password" => ep}) 
+            {:ok, u} = Repo.insert(%User{uid: s, email: e, password: p}) 
         else # Respond With Error
             {:error, "Account already registered with that email."}
         end
