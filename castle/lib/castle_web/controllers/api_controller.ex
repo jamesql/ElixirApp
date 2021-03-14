688e657056abeaca5d042631015f2307bb0bff0c
James <thejamesash1@gmail.com>
2021-03-13 20:21:58 -0500

    start finishing authentication backend

--- castle/lib/castle_web/controllers/api_controller.ex
+++ castle/lib/castle_web/controllers/api_controller.ex
@@ -1,12 +1,17 @@
 defmodule CastleWeb.APIController do
   use CastleWeb, :controller
 
+  alias Castle.Accounts
+
   def login(conn, params) do
     case params do
-        %{"username" => username, "password" => password} -> 
+        %{"email" => email, "password" => password} -> # Regular API Call
             conn
             |> json(%{id: 123})
-        _ ->
+        %{"_csrf_token"=> t, "body"=> %{"email"=> email, "password"=> password}} -> # Browser Request
+            conn
+            |> json(%{id: 222})
+        _ -> # Invalid Payload
             conn 
             |> send_resp(400, "Invalid Payload.")
     end
